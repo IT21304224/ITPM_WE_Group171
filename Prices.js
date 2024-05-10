c8902b039be66ca9db2ca0d0d0c1a0f93062e78f
Wijesinghe K.S.K <101441374+kithminiwijesinghe@users.noreply.github.com>
2024-05-11 01:08:20 +0530

    Add prices
    Prices added successfully

---
+++ Prices.js
@@ -0,0 +1,32 @@
+export const Prices = [
+  {
+    _id: 0,
+    name: "LKR 0 to 1000",
+    array: [0, 999],
+  },
+  {
+    _id: 1,
+    name: "LKR 1000 to 1500",
+    array: [1000, 1499],
+  },
+  {
+    _id: 2,
+    name: "LKR 1500 to 2000",
+    array: [1500, 2000],
+  },
+  {
+    _id: 3,
+    name: "LKR 2000 to 2500",
+    array: [2000, 2500],
+  },
+  {
+    _id: 4,
+    name: "LKR 2500 to 3000",
+    array: [2500, 3000],
+  },
+  {
+    _id: 4,
+    name: "LKR 3000 or more ",
+    array: [3000, 10000],
+  },
+];
