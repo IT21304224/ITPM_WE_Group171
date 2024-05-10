95047280178a26a437b784ee93009a0f26b20a6c
AmandaY <99309637+IT21163272@users.noreply.github.com>
2024-05-10 21:13:14 +0530

    Add files via upload

---
+++ Summary.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../../Components/Header";
+import SideBar2 from "../../Components/SideBar2";
+// import MaterialContent from "../../Components/InventoryManagement/MaterialContent";
+
+function Summary() {
+  return (
+    <div>
+      <Header />
+      <SideBar2 />
+      {/* <MaterialContent /> */}
+    </div>
+  );
+}
+
+export default Summary;
