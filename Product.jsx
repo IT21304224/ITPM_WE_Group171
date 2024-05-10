84eae549cc71dec09319108242614d79996b53f5
AmandaY <99309637+IT21163272@users.noreply.github.com>
2024-05-10 21:12:55 +0530

    adding product details

---
+++ Product.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../../Components/Header";
+import SideBar from "../../Components/SideBar";
+import ProductContent from "../../Components/InventoryManagement/ProductContent";
+
+function Product() {
+  return (
+    <div>
+      <Header />
+      <SideBar />
+      <ProductContent />
+    </div>
+  );
+}
+
+export default Product;
---
+++ productp.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../../Components/Header";
+import SideBar1 from "../../Components/SideBar1";
+import ProductContent from "../../Components/InventoryManagement/ProductContent";
+
+function Productp() {
+  return (
+    <div>
+      <Header />
+      <SideBar1 />
+      <ProductContent />
+    </div>
+  );
+}
+
+export default Productp;
