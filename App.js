d110a870806b40cb85246c31121766aaca3ea74f
Wijesinghe K.S.K <101441374+kithminiwijesinghe@users.noreply.github.com>
2024-05-11 02:01:47 +0530

    Add structure of the web page
    Main structure of the application added successfully

---
+++ App.js
@@ -0,0 +1,56 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Policy from "./pages/Policy";
+import Pagenotfound from "./pages/Pagenotfound";
+import Register from "./pages/Auth/Register";
+import Login from "./pages/Auth/Login";
+import Dashboard from "./pages/user/Dashboard";
+import PrivateRoute from "./components/Routes/Private";
+import ForgotPasssword from "./pages/Auth/ForgotPasssword";
+import AdminRoute from "./components/Routes/AdminRoute";
+import AdminDashboard from "./pages/Admin/AdminDashboard";
+import Profile from "./pages/user/Profile";
+import Search from "./pages/Search";
+import ProductDetails from "./pages/ProductDetails";
+import CartPage from "./pages/CartPage";
+import AdminOrders from "./pages/Admin/AdminOrders";
+import Exchange from "./pages/exchange";
+import AdminFeedbacks from "./pages/Admin/AdminFeedback";
+
+
+
+//.....................................................................................................................................................................
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/product/:slug" element={<ProductDetails />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/dashboard" element={<PrivateRoute />}>
+          <Route path="user" element={<Dashboard />} />
+          <Route path="user/profile" element={<Profile />} />
+        </Route>
+        <Route path="/dashboard" element={<AdminRoute />}>
+          <Route path="admin" element={<AdminDashboard />} />
+          <Route path="admin/badge-holders" element={<AdminOrders />} />
+          <Route path="admin/feedbacks" element={<AdminFeedbacks />} />
+        </Route>
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPasssword />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/exchange" element={<Exchange />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/policy" element={<Policy />} />
+        <Route path="*" element={<Pagenotfound />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
